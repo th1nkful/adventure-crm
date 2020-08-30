da0561290de06770cb84a8eae3220788857fcedb
“Joshua <punshon.jm@live.com.au>
2020-08-30 22:26:34 +1000

    add empty migrations, update volunteers model

---
+++ src/server/migrations/008_create_Competitors.js
@@ -0,0 +1,8 @@
+module.exports = {
+  up: () => {
+
+  },
+  down: () => {
+
+  },
+};
--- src/server/models/Volunteers.js
+++ src/server/models/Volunteers.js
@@ -4,11 +4,22 @@ module.exports = (sequelize) => {
   class Volunteers extends Model {}
 
   Volunteers.init({
-
+    time: {
+      type: DataTypes.STRING,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
   }, {
     sequelize,
     tableName: 'Volunteers',
   });
+
+  Volunteers.associate = ({
+    Events,
+  }) => {
+    Volunteers.event = Volunteers.belongsTo(Events, { foreignKey: 'eventId' });
+  };
 
   return Volunteers;
 };
